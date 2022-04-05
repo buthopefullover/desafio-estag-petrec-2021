b7db6d54c8587f0d371cb1afcc1522eb58f3c013
victoria <victoria.set.1999@gmail.com>
2022-04-05 18:55:51 -0300

    add mensagem em home

--- petrec/petrec/settings.py
+++ petrec/petrec/settings.py
@@ -25,7 +25,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&3d45wy@%v*ufp#+(1_4*lg3c2j1k=#0xet_6phji81@)5p@2'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
